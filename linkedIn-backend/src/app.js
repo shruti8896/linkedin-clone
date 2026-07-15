fcbce33396b21c739ff93db7e5c7aa746117c600
Shruti <shruti.substring@gmail.com>
2026-07-15 18:48:12 +0530

    Remove trailing slash from vercel origin in app.js CORS config

--- linkedIn-backend/src/app.js
+++ linkedIn-backend/src/app.js
@@ -19,7 +19,7 @@ export const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://linkedin-clone-delta-three.vercel.app/",
+      "https://linkedin-clone-delta-three.vercel.app",
     ],
     credentials: true,
   },
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://linkedin-clone-delta-three.vercel.app/",
+      "https://linkedin-clone-delta-three.vercel.app",
     ],
     credentials: true,
   }),
